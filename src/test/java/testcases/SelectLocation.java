02666b86f43abb37c3b9ef5f9b68387be56d4957
panchalmauik28 <panchalmaulik28@gmail.com>
2025-02-25 23:51:33 +0530

    WhereShouldWeDeliver

--- src/test/java/testcases/SelectLocation.java
+++ src/test/java/testcases/SelectLocation.java
@@ -3,12 +3,16 @@ package testcases;
 import org.testng.annotations.Test;
 
 import base.Base;
+import pages.WhereShouldWeDeliver;
 
 public class SelectLocation extends Base {
 
+	WhereShouldWeDeliver whereShouldWeDeliver;
+
 	@Test
 	public void whereShouldWeDeliver() {
-		
+		whereShouldWeDeliver = new WhereShouldWeDeliver();
+		whereShouldWeDeliver.selectArea("380013", "Nava Vadaj, Ahmedabad");
 	}
-	
+
 }
